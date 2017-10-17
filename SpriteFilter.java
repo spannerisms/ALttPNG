9983d899ae09464ec57f0d6631461f593fc2dcc7
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-10-17 12:26:29 -0400

    Add files via upload
    Remove useless functions
    Sprite and palette reading (still clean up to do)

--- SpriteFilter.java
+++ SpriteFilter.java
@@ -1,5 +1,10 @@
+import java.awt.image.DataBufferByte;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class SpriteFilter {
@@ -19,13 +24,32 @@ public class SpriteFilter {
 			{4,2},{4,3},{5,2},{5,3},{6,2},{6,3},{7,2},{7,3}
 	};
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 	}
 	
 	/**
-	 * 
+	 * Reads a sprite file
+	 * @throws IOException 
 	 */
+	public static byte[] readSprite(String path) throws IOException {
+		File file = new File(path);
+		byte[] ret = new byte[(int) file.length()];
+		FileInputStream s;
+		try {
+			s = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			throw e;
+		}
+		try {
+			s.read(ret);
+			s.close();
+		} catch (IOException e) {
+			throw e;
+		}
+		
+		return ret;
+	}
 	/**
 	 * Takes a sprite and turns it into 896 blocks of 8x8 pixels
 	 * @param sprite
@@ -51,14 +75,14 @@ public class SpriteFilter {
 			
 			// byte to unravel
 			byte q = sprite[i];
-			
+			System.out.println(r + " " + p);
 			// run through the byte
 			for (int c = 0; c < 8; c++) {
 				// AND with 1 shifted to the correct plane
-				boolean bitOn = (q & (1 << (7-c))) == 1;
+				boolean bitOn = (q & (1 << (7-c))) != 0;
 				// if true, OR with that plane in index map
 				if (bitOn)
-					ret[b][r][c] |= (1 << (3-p));
+					ret[b][r][c] |= (1 << (p));
 			}
 		}
 		return ret;
@@ -67,9 +91,13 @@ public class SpriteFilter {
 	/**
 	 * Read palette from last set of data
 	 */
-	public static int[] getPalette(byte[] palette) {
-		int[] pal = new int[64];
-		
+	public static byte[] getPalette(byte[] sprite) {
+		byte[] pal = new byte[PALETTESIZE];
+		int offset = SPRITESIZE;
+		for (int i = 0; i < PALETTESIZE; i++) {
+			pal[i] = sprite[offset+i];
+			System.out.println(pal[i]);
+		}
 		return pal;
 	}
 	/**
@@ -119,72 +147,6 @@ public class SpriteFilter {
 	}
 
 	/**
-	 * Turn the image into an array of 8x8 blocks.
-	 * Assumes ABGR color space.
-	 * <br><br>
-	 * If a color matches an index that belongs to one of the latter 3 mails
-	 * but does not match anything in green mail
-	 * then it is treated as the color at the corresponding index of green mail.
-	 * 
-	 * @param pixels - aray of color indices
-	 * @param pal - palette colors
-	 * @return <b>byte[][][]</b> representing the image as a grid of color indices
-	 */
-	public static byte[][][] rasterTo8x8(byte[] pixels, int[] pal) {
-		int dis = pixels.length/4;
-		int largeCol = 0;
-		int intRow = 0;
-		int intCol = 0;
-		int index = 0;
-
-		// all 8x8 squares, read left to right, top to bottom
-		byte[][][] eightbyeight = new byte[896][8][8];
-
-		// read image
-		for (int i = 0; i < dis; i++) {
-			// get each color and get rid of sign
-			// colors are stored as {A,B,G,R,A,B,G,R...}
-			int b = (pixels[i*4+1]+256)%256;
-			int g = (pixels[i*4+2]+256)%256;
-			int r = (pixels[i*4+3]+256)%256;
-
-			// convert to 9 digits
-			int rgb = (1000000 * r) + (1000 * g) + b;
-
-			// find palette index of current pixel
-			for (int s = 0; s < pal.length; s++) {
-				   if (pal[s] == rgb) {
-					eightbyeight[index][intRow][intCol] = (byte) (s % 16); // mod 16 in case it reads another mail
-					break;
-				}
-			}
-
-			// count up square by square
-			// at 8, reset the "Interior column" which we use to locate the pixel in 8x8
-			// increments the "Large column", which is the index of the 8x8 sprite on the sheet
-			// at 16, reset the index and move to the next row
-			// (so we can wrap around back to our old 8x8)
-			// after 8 rows, undo the index reset, and move on to the next super row
-			intCol++;
-			if (intCol == 8) {
-				index++;
-				largeCol++;
-				intCol = 0;
-				if (largeCol == 16) {
-					index -= 16;
-					largeCol = 0;
-					intRow++;
-					if (intRow == 8) {
-						index += 16;
-						intRow = 0;
-					}
-				}
-			}
-		}
-		return eightbyeight;
-	}
-
-	/**
 	 * Converts an index map into a proper 4BPP (SNES) byte map.
 	 * @param eightbyeight - color index map
 	 * @param pal - palette
@@ -230,7 +192,7 @@ public class SpriteFilter {
 		// end 4BPP
 
 		// add palette data, starting at end of sheet
-		int i = SPRITESIZE-2;
+		int i = SPRITESIZE;
 		for (byte b : palData) {
 			if (i == bytemap.length)
 				break;
